ba65a2042a9abeff50700ad19d346e8cff59b18b
Haowei-Oh <haoweioh123@email.tamu.edu>
2025-11-05 14:26:19 -0600

    Created additional functions
    - Created testQuery
    - Refactored connectDB

--- javascript/db.js
+++ javascript/db.js
@@ -1,50 +1,57 @@
-//Dedicated file for database connection and functions related to pulling or calling from database
-
-//To be organized Soon
-
-// db.js
 const { Pool } = require('pg');
 require('dotenv').config({path: require('path').resolve(__dirname, '../.env')});
 
-const pool = new Pool({
-    host: 'csce-315-db.engr.tamu.edu',
-    port: 5432, // default PostgreSQL port
-    user: process.env.DBUSER,
-    password: process.env.DBPASSWORD,
-    database: 'CSCE315Database',
-    ssl: { rejectUnauthorized: false }
-});
+//Dedicated file for all database connections (until further notice)
 
+var pool;
 var usersArray;
+var isConnected = false;
 
-//Async essentially breaks a thread off and allows it to run as long as it needs to
-//In this case, if it take like 5 minutes to connect for some reason, everything in test.js won't be held up by it
-//Allows the use of "await", which seems to force the thread to wait until the action is complete before proceeding
+//Async makes something into a Promise. Now I have to double check how to run asynchronous programming stuff again :/
 /**
- * Establishes connection to the database and runs a query
- * 
- * TO BE UPDATED TO BE MORE GENERIC
+ * Establishes connection to the database
  */
 async function connectDB() {
     try {
+        //Try to connect
+        pool = new Pool({
+            host: 'csce-315-db.engr.tamu.edu',
+            port: 5432, // default PostgreSQL port
+            user: process.env.DBUSER,
+            password: process.env.DBPASSWORD,
+            database: 'CSCE315Database',
+            ssl: { rejectUnauthorized: false }
+        });
+        console.log('Pool created. Testing connection');
+        await pool.query('SELECT 1');
         console.log('✅ Connected to PostgreSQL database');
-        // Example query
-        //Logic: each row is literally 1 entry in the table
-        //KeyValue Pairs. Or just output the row directly I guess
-        const res = await pool.query('SELECT * FROM menuce');
-        console.log('Row 1:', res.rows[0]);
-        //From rows, map each element "row" and apply the transform "row.name" (getting row name)
-        //Might be missing a thing, but this method can be used for a lot of things it seems
-        //Unneccessary. Can directly access the attribute name it seems
-        const extractedData = res.rows.map(row => row.name);
-        console.log('Extracted names only: ', extractedData[0]);
-        //Works
-        console.log('Directedly extracted name: ', res.rows[0].name);
+        isConnected = true;
     } catch (err) {
         console.error('❌ Connection error:', err.stack);
     } finally {
         await pool.end();
     }
+}
+
+//Not to be actually used. Use this as a test site for all connections
+async function testQuery()
+{
+    if(!isConnected)
+    {
+        connectDB();
+    }
+    // Example query
+    //Logic: each row is literally 1 entry in the table
+    //KeyValue Pairs. Or just output the row directly I guess
+    const res = await pool.query('SELECT * FROM menuce');
+    console.log('Row 1:', res.rows[0]);
+    //From rows, map each element "row" and apply the transform "row.name" (getting row name)
+    //Might be missing a thing, but this method can be used for a lot of things it seems
+    //Unneccessary. Can directly access the attribute name it seems
+    const extractedData = res.rows.map(row => row.name);
+    console.log('Extracted names only: ', extractedData[0]);
+    //Works
+    console.log('Directedly extracted name: ', res.rows[0].name);
 }
 
 /**
@@ -126,5 +133,6 @@ module.exports = {
     query: (text, params) => pool.query(text, params), // generic helper
     connectDB,
     validateUser,
-    addOrders
+    addOrders,
+    deleteMenuItem
 };
