986cecb1ddffc71ada33cfadf74c5a22c1ee6db4
jhineman-tamu <jachinema@tamu.edu>
2025-11-09 20:24:44 -0600

    Delete javascript directory

--- javascript/test.js
+++
@@ -1,8 +0,0 @@
-//Command to run: node main.js (for a file called "main.js")
-
-//Object that contains the connection protocol
-//This is the testing site.
-const dbConn = require('./db');
-dbConn.connectDB();
-
-//console.log("completion:");
