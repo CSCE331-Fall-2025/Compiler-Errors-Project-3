032c5e832d346da015221337912fa19547a3ad9e
EHP0 <EHPO@tamu.edu>
2025-11-19 11:14:28 -0600

    Got dynamic menu working for cashier

--- app/src/components/CashierMenuPanel.jsx
+++ app/src/components/CashierMenuPanel.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CashierMenuItem from './CashierMenuItem';
 
 function CashierMenuPanel(){
+    const [data, setData] = useState([]);
+    
+        useEffect(() => {
+        // change to async for real implementation, and uncomment
+        // "async function getMenu()"
+        async function getMenu() {
+            const response = await fetch('http://localhost:3000/api/OrderMenu/fetchMenu');
+            const data = await response.json();
+            console.log
+            setData(data);
+            // const data = [
+            //     {
+            //         img: '/images/Orange Chicken.png',
+            //         altText: 'orng chikn',
+            //         title: 'Orange Chicken',
+            //         cal: '150 cal',
+            //         price: '$3.99',
+            //         link: 'customize.html',
+            //     }, 
+            // ];
+            // setData(data);
+        }
+        getMenu();
+    }, []);
     return(
-        <section class = "menu-panel">
-            <h1>Menu Items</h1>
-            <div id = "menuItems" class = "menuItems">
-                {/* items will be added here or served by server side */}
-                <div class = "empty">Your cart is empty</div>
-            </div>
+        <section class="menu-panel">
+            {data.map((item) => (
+                <CashierMenuItem key={item.title} {...item} />
+            ))}
         </section>
     );
 }
