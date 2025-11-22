7cbb902ca7c99500ec0180453fde4a8d7e9eef25
root <root@AK.localdomain>
2025-11-22 16:16:22 -0600

    cleaned checkout css and added some spacing in style css

--- app/src/components/Hero.jsx
+++ app/src/components/Hero.jsx
@@ -11,19 +11,18 @@ function Hero() {
                     <Link to="/order">Order Now</Link>
                 </div>
             </section>
+
+            {/* section bottom one below */}
             <section className="popular-section">
                 <div className="popular-content-grid">
-                    {/* Left Content */}
                     <div className="popular-left">
                         <h2 className="popular-title">Popular item</h2>
                         <p className="popular-description">
                             Crispy chicken tossed in sweet and tangy orange sauce.
                         </p>
                         <p className="popular-review">Customer Review : </p>      
-                        <p className="popular-review">I don't understand the bad reviews, place is great, workers were very kind, good ambiance, and the best food out of every Panda Express I've been to.</p>
+                        <p className="popular-review">I don't understand the bad reviews, place is great, workers were very kind, good ambiance, and the best food out of every Parody Express I've been to.</p>
                     </div>
-
-                    {/* Right Content */}
                     <div className="popular-right">
                         <div className="popular-image-container">
                             <img
--- app/src/components/customer/order/OrderPreview.jsx
+++ app/src/components/customer/order/OrderPreview.jsx
@@ -6,7 +6,7 @@ function OrderPreview() {
     return (
         <div class="order-preview-details">
             <div class="order-preview-title">
-             <h1>Your Order</h1>
+             <h1> YOUR ORDER </h1>
             </div>
             <OrderPreviewItems></OrderPreviewItems>
         </div>
